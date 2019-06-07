f7cc369f3e9c8510e5b5639df41a574119729f94
Alexis <texs.mv@gmail.com>
2019-06-07 00:09:58 -0500

    main

--- src/main.cu
+++ src/main.cu
@@ -10,9 +10,9 @@
 
 int map_n_ratings, map_n_users, map_n_movies;
 int n_ratings, n_users, n_movies;
-int n_ratings_20, n_users_20, n_ratings_27, n_users_27, n_movies_27, n_ratings_l, n_users_l, n_movies_l, n_ratings_l2, n_users_l2, n_movies_l2;
-int max_users = 300000;
-int max_movies = 200000;
+int n_ratings_20, n_users_20, n_ratings_27, n_users_27, n_movies_27, n_ratings_l, n_users_l, n_movies_l, n_ratings_l2, n_users_l2, n_movies_l2, n_users_lol_17, n_ratings_lol_17;
+int max_users = 8000;
+int max_movies = 200;
 
 map<int, map<int, float>* > map_users;
 map<int, map<int, float>* > map_items;
@@ -45,6 +45,22 @@ float * d_averages;
 
 
 
+vector<int> recomentar_equipo(  vector<int> ids_champions){
+  RBTree<int, float>** d_n_map;
+  create_map_cuda(ids_champions, d_n_map);
+
+  reloj te;
+  te.start();
+  vector<int> recomendacion = knns_cosine(d_map_users, d_n_map, d_row_size, max_users, 5);
+  te.stop();
+  cout<<"Tiempo de prediccion: "<< te.time()<<endl;
+
+  delete_map_cuda(d_n_map);
+  return recomendacion;
+
+}
+
+
 
 int main(int argc, char const *argv[]) {
 
@@ -66,11 +82,14 @@ int main(int argc, char const *argv[]) {
   n_ratings_l2 = 21;
   n_users_l2 = 5;
 
+  n_users_lol_17 = 3117;
+  n_ratings_lol_17 = 31170;
+
   // n_ratings = n_ratings_20;
   // n_users = n_users_20;
 
-  n_ratings = n_ratings_l;
-  n_users = n_users_l;
+  n_ratings = n_ratings_lol_17;
+  n_users = n_users_lol_17;
   // n_movies = n_movies_27;
 
   // n_ratings = n_ratings_l2;
@@ -80,8 +99,8 @@ int main(int argc, char const *argv[]) {
 
 
   // n_ratings
-  // n_of_users("../databases/libro/ratings.csv", n_ratings, n_users, true);
-  // cout<<n_ratings<<" "<<n_users<<endl;
+  n_of_users("dataset/HeroPart2.csv", n_ratings, n_users, false);
+  cout<<n_ratings<<" "<<n_users<<endl;
 
 
   d_values = cuda_array<float>(n_ratings);
@@ -111,15 +130,19 @@ int main(int argc, char const *argv[]) {
 
   reloj a;
   a.start();
-  read_ML_ratings("dataset/ratings.csv", n_ratings, n_users, true, values, row_ind, col_ind, ind_users, row_size, "l");
-  read_ML_ratings_items("dataset/ratings.csv", n_ratings, n_users, max_movies, true,  item_values,  item_row_ind,  item_col_ind,  ind_items, item_row_size, "l");
+  // read_ML_ratings("dataset/ratings.csv", n_ratings, n_users, true, values, row_ind, col_ind, ind_users, row_size, "l");
+  // read_ML_ratings_items("dataset/ratings.csv", n_ratings, n_users, max_movies, true,  item_values,  item_row_ind,  item_col_ind,  ind_items, item_row_size, "l");
+  read_ML_ratings("dataset/HeroPart2.csv", n_ratings, n_users, max_users, false, values, row_ind, col_ind, ind_users, row_size, "_2017");
+  read_ML_ratings_items("dataset/HeroPart2.csv", n_ratings, n_users, max_movies, false,  item_values,  item_row_ind,  item_col_ind,  ind_items, item_row_size, "_2017");
   a.stop();
   cout<<"Tiempo de carga de bd: "<<a.time()<<"ms"<<endl;
   // read_ML_ratings("../collaborative_filtering/databases/libro/ratings.csv", n_ratings, n_users, true, values, row_ind, col_ind, ind_users, row_size, "l");
   // read_ML_ratings_items("../collaborative_filtering/databases/libro/ratings.csv", n_ratings, n_users, max_movies, true,  item_values,  item_row_ind,  item_col_ind,  ind_items, item_row_size, "l");
 
 
-  average_per_user(values, ind_users, row_size, maxs, mins, averages, max_users);
+
+
+  // average_per_user(values, ind_users, row_size, maxs, mins, averages, max_users);
 
 
 
@@ -147,9 +170,26 @@ int main(int argc, char const *argv[]) {
 
   create_maps_device(d_map_users, d_values, d_row_ind, d_col_ind, d_ind_users, d_row_size, max_users);
 
-  knns_cosine(d_map_users, d_row_size, 1, max_users, 5);
 
 
+
+
+
+
+
+
+// Esto es todo lo que se necesita para recomendar
+
+  vector<int> ids_champions = {2, 8};
+
+
+  vector<int> recomendacion = recomentar_equipo(ids_champions);
+
+  for (size_t i = 0; i < recomendacion.size(); i++) {
+    cout<<recomendacion[i]<<endl;
+  }
+
+// -----------------------------------------------
 
 
 
